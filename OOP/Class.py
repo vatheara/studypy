d8558d01be0f52aa690d50806b6c57b245e21c7d
Noobie <theara2048@gmail.com>
2020-09-12 13:54:06 +0700

    add OPP

---
+++ OOP/Class.py
@@ -0,0 +1,7 @@
+#define class
+class Dog:
+    var = 123
+    species = "Canis familiaris"
+    def __init__(self, name, age):
+        self.name = name
+        self.age  = age
---
+++ OOP/class.py
@@ -0,0 +1,7 @@
+#define class
+class Dog:
+    var = 123
+    species = "123 familiaris"
+    def __init__(self, name, age):
+        self.name = name
+        self.age  = age
